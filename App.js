8b5a13327cf33cbf728e2424a20cc5014ed15b66
austin-9903 <austin.a@zohocorp.com>
2025-08-16 21:39:33 +0530

    Tried component composition

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-var content = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "This is h1 tag!"), React.createElement("h2", {}, "This is h3 tag!")]));
-console.log('content: ', content);
-var root = ReactDOM.createRoot(document.getElementById("root"));
+let Title = () => <h1>
+    Hi! I am the Title
+</h1>;
 
-root.render(content);
+let Content = () => <div>
+    <Title />
+    <h2>Hi! I am the Subtitle</h2>
+</div>;
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Content />);
