18fd01336c6d674ade6797334a5ad763186c942c
austin-9903 <austin.a@zohocorp.com>
2025-08-16 11:45:59 +0530

    Installed parcel and imported react from package

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-var content = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "This is h1 tag!"), React.createElement("h2", {}, "This is h2 tag!")]));
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+var content = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "This is h1 tag!"), React.createElement("h2", {}, "This is h3 tag!")]));
 console.log('content: ', content);
 var root = ReactDOM.createRoot(document.getElementById("root"));
 
